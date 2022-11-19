03d569659b98711d7fffd938f4bfcfb592e26b13
Aristylux <axelmezade@orange.fr>
2022-11-19 15:45:05 +0100

    create gogocar

--- GoGoCar/app/src/main/java/com/aristy/gogocar/SplashActivity.java
+++ GoGoCar/app/src/main/java/com/aristy/gogocar/SplashActivity.java
@@ -2,12 +2,24 @@ package com.aristy.gogocar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 1100);
     }
 }
