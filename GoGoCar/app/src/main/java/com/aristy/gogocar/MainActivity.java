9aa87c188decfec43eaa9f266ee5efb06d1ec9b3
Aristylux <axelmezade@orange.fr>
2022-11-25 18:13:26 +0100

    add swipe in settings

--- GoGoCar/app/src/main/java/com/aristy/gogocar/MainActivity.java
+++ GoGoCar/app/src/main/java/com/aristy/gogocar/MainActivity.java
@@ -1,16 +1,11 @@
 package com.aristy.gogocar;
 
-import static com.aristy.gogocar.CodesTAG.TAG_Web;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.Window;
@@ -43,15 +38,14 @@ public class MainActivity extends AppCompatActivity {
         // Interface
         web.addJavascriptInterface(new WebInterface(this, this, web), "Android");
 
+        // For top bar and navigation bar
         // In Activity's onCreate() for instance
         if (Build.VERSION.SDK_INT >= 19 && Build.VERSION.SDK_INT < 21) {
             setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, true);
         }
-
         if (Build.VERSION.SDK_INT >= 19) {
             getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR | View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR);
         }
-
         if (Build.VERSION.SDK_INT >= 21) {
             setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false);
         }
