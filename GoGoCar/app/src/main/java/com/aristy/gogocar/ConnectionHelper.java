14b34734627e90354ba591e3639b498652ebd7ee
Aristylux <axelmezade@orange.fr>
2022-12-13 18:53:59 +0100

    clean up connectionHelper

--- GoGoCar/app/src/main/java/com/aristy/gogocar/ConnectionHelper.java
+++ GoGoCar/app/src/main/java/com/aristy/gogocar/ConnectionHelper.java
@@ -8,35 +8,36 @@ import android.util.Log;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
+/**
+ * ConnectionHelper:
+ * <p>
+ *     Establish connection to database
+ * </p>
+ */
 public class ConnectionHelper {
 
-    private String host;
+    private final static String host = "192.168.1.187";
     private final static String port = "5432";
-    private String databaseName;
+    private final static String databaseName = "gogocar";
     private final static String userName = "postgres";
     private final static String password = "password";
 
     public Connection openConnection() {
-
-        host = "192.168.1.187";
-        databaseName = "gogocar";
-
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
 
         Connection connection = null;
         String jdbcURL = "jdbc:postgresql://" + host + ":" + port + "/" + databaseName;
         Log.d(TAG_Database, "ConnectionClass: " + jdbcURL);
-
         try {
+            // Get connection to the database
             connection = DriverManager.getConnection(jdbcURL, userName, password);
             Log.d(TAG_Database, "ConnectionClass: Connected");
         } catch (Exception exception) {
-            Log.e(TAG_Database, "openConnection: ", exception);
+            Log.e(TAG_Database, "ConnectionClass: ", exception);
             exception.printStackTrace();
         }
 
         return connection;
-
     }
 }
