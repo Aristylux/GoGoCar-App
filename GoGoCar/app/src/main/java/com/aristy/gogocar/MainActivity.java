6874cd84f9b05de2b6171efaf8d23dae258bf30f
Aristylux <axelmezade@orange.fr>
2022-12-13 19:32:38 +0100

    add valid test

--- GoGoCar/app/src/main/java/com/aristy/gogocar/MainActivity.java
+++ GoGoCar/app/src/main/java/com/aristy/gogocar/MainActivity.java
@@ -3,15 +3,12 @@ package com.aristy.gogocar;
 import static com.aristy.gogocar.CodesTAG.TAG_Auth;
 import static com.aristy.gogocar.CodesTAG.TAG_Database;
 import static com.aristy.gogocar.CodesTAG.TAG_Debug;
-import static com.aristy.gogocar.CodesTAG.TAG_Info;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.content.SharedPreferences;
-import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -83,19 +80,25 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
+        // If the connection to the server is close, open it
+        if (connectionValid()) {
+            Log.d(TAG_Database, "onStart: open SQL Connection");
+            SQLConnection = connectionHelper.openConnection();
+        } else {
+            Log.e(TAG_Database, "onStart: ERROR open SQL Connection: invalid");
+        }
+    }
+
+    public boolean connectionValid(){
         try {
-            // If the connection to the server is close, open it
-            if (SQLConnection != null) {
-                if (SQLConnection.isClosed()) {
-                    Log.d(TAG_Database, "onStart: open SQL Connection");
-                    SQLConnection = connectionHelper.openConnection();
-                }
-            } else {
-                Log.e(TAG_Database, "onStart: ERROR open SQL Connection: null");
-            }
+            if (SQLConnection != null)
+                return !SQLConnection.isClosed();
+            else
+                return false;
         } catch (SQLException exception) {
-            Log.e(TAG_Database, "onStart: ERROR open SQL Connection", exception);
+            Log.e(TAG_Database, "connectValid: ", exception);
             exception.printStackTrace();
+            return false;
         }
     }
 
@@ -105,11 +108,11 @@ public class MainActivity extends AppCompatActivity {
         super.onStop();
         try {
             // The user leave application, close connection to the server.
-            if (SQLConnection != null) {
+            if (connectionValid()) {
                 Log.d(TAG_Debug, "onStop: close SQL connection");
                 SQLConnection.close();
             } else {
-                Log.e(TAG_Debug, "onStop: ERROR close SQL connection: null");
+                Log.e(TAG_Debug, "onStop: ERROR close SQL connection: invalid");
             }
         } catch (SQLException exception) {
             Log.e(TAG_Debug, "onStop: ERROR close SQL connection: ", exception);
@@ -118,28 +121,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void setWindowVersion(){
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT && Build.VERSION.SDK_INT <  Build.VERSION_CODES.LOLLIPOP) {
-            Log.d(TAG_Debug, "setWindowVersion: 1");
-            setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, true);
-        }
-        if (Build.VERSION.SDK_INT >=  Build.VERSION_CODES.KITKAT) {
-            Log.d(TAG_Debug, "setWindowVersion: 2");
-            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR | View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR);
-        }
-        if (Build.VERSION.SDK_INT >=  Build.VERSION_CODES.LOLLIPOP) {
-            Log.d(TAG_Debug, "setWindowVersion: 3");
-            setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false);
-        }
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR | View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR);
+        setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
     }
 
-    public static void setWindowFlag(Activity activity, final int bits, boolean on) {
+    public static void setWindowFlag(Activity activity, final int bits) {
         Window win = activity.getWindow();
         WindowManager.LayoutParams winParams = win.getAttributes();
-        if (on) {
-            winParams.flags |= bits;
-        } else {
-            winParams.flags &= ~bits;
-        }
+        winParams.flags &= ~bits;
         win.setAttributes(winParams);
     }
 
