3f7ddd22b2c9cd6f8a87c08d1483685c99103472
Aristylux <axelmezade@orange.fr>
2022-12-20 18:09:55 +0100

    init mofif project to add fragment

--- GoGoCar/app/src/main/java/com/aristy/gogocar/MainActivity.java
+++ GoGoCar/app/src/main/java/com/aristy/gogocar/MainActivity.java
@@ -33,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
 
         // Connect to database (do it in other thread)
         connectionHelper = new ConnectionHelper();
