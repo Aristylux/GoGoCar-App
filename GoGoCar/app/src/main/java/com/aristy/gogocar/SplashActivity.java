d5f47c58fc167b3d7e6f04417e148fc09e0093c6
Aristylux <axelmezade@orange.fr>
2022-12-23 11:07:40 +0100

    add init connection in splash activity

--- GoGoCar/app/src/main/java/com/aristy/gogocar/SplashActivity.java
+++ GoGoCar/app/src/main/java/com/aristy/gogocar/SplashActivity.java
@@ -8,7 +8,10 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Parcelable;
 import android.util.Log;
+
+import java.sql.Connection;
 
 @SuppressLint("CustomSplashScreen")
 public class SplashActivity extends AppCompatActivity {
@@ -28,10 +31,14 @@ public class SplashActivity extends AppCompatActivity {
         }, 1100);
 
         // Operations
+        // Connect to database (do it in other thread)
+        ConnectionHelper connectionHelper = new ConnectionHelper();
+        Connection SQLConnection = connectionHelper.openConnection();
 
         UserSharedPreference userdata = new UserSharedPreference(SplashActivity.this);
         int userID = userdata.readUserID();
         intent.putExtra("USER_ID", userID);
+        intent.putExtra("CONNECTION", (Parcelable) SQLConnection);
 
         //https://stackoverflow.com/questions/2139134/how-to-send-an-object-from-one-android-activity-to-another-using-intents
 
