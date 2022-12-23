ca925754f18fe99cde7c85553b8a414092993200
Aristylux <axelmezade@orange.fr>
2022-12-23 16:35:26 +0100

    add comments

--- GoGoCar/app/src/main/java/com/aristy/gogocar/SplashActivity.java
+++ GoGoCar/app/src/main/java/com/aristy/gogocar/SplashActivity.java
@@ -20,21 +20,25 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Log.d(TAG_SPLASH, "onCreate: splash");
-        Intent intent =new Intent(SplashActivity.this, MainActivity.class);
+        Log.d(TAG_SPLASH, "SPLASH: START");
 
+        // Create Intent
+        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+
+        // After 1.1 second, Launch app
         new Handler().postDelayed(() -> {
             // This method will be executed once the timer is over
-            //overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
+            overridePendingTransition(R.anim.to_left, R.anim.from_right);
             startActivity(intent);
             finish();
         }, 1100);
 
         // Operations
-
         UserPreferences userPreferences = new UserPreferences(SplashActivity.this);
+
         // Retrieve user from data in app
         userPreferences.readUser();
+
         // Get user id (by default (unset) int=0, first element in database by default: 1)
         int userID = userPreferences.getUserID();
         boolean isLogged = userID != 0;
@@ -43,9 +47,6 @@ public class SplashActivity extends AppCompatActivity {
         intent.putExtra("IS_USER_LOGGED", isLogged);
         intent.putExtra("USER", userPreferences);
 
-        //https://stackoverflow.com/questions/2139134/how-to-send-an-object-from-one-android-activity-to-another-using-intents
-
-        Log.d(TAG_SPLASH, "onCreate: end");
-
+        Log.d(TAG_SPLASH, "SPLASH: END");
     }
 }
