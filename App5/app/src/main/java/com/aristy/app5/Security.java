675fb0058ccc49a9666de189acaa839581d901d0
Aristylux <axelmezade@orange.fr>
2023-01-10 21:34:14 +0100

    code security class

--- App5/app/src/main/java/com/aristy/app5/Security.java
+++ App5/app/src/main/java/com/aristy/app5/Security.java
@@ -1,4 +1,38 @@
 package com.aristy.app5;
 
+import java.security.Key;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+
 public class Security {
+
+    private static final String ALGORITHM = "AES";
+    private static final byte[] KEY = "MySuperSecretKey".getBytes();
+    Key key = new SecretKeySpec(KEY, ALGORITHM);
+
+    public String encrypt(String plainText) {
+        try {
+            Cipher cipher = Cipher.getInstance(ALGORITHM);
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+            byte[] encryptedText = cipher.doFinal(plainText.getBytes());
+            return DatatypeConverter.printBase64Binary(encryptedText);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public String decrypt(String encryptedText) {
+        try {
+            Cipher cipher = Cipher.getInstance(ALGORITHM);
+            cipher.init(Cipher.DECRYPT_MODE, key);
+            byte[] decryptedText = cipher.doFinal(DatatypeConverter.parseBase64Binary(encryptedText));
+            return new String(decryptedText);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 }
