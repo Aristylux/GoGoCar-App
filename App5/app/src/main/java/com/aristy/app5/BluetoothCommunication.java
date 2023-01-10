78d2434615abb26d5a90403aafb5155818b88303
Aristylux <axelmezade@orange.fr>
2023-01-10 18:45:53 +0100

    implement communication and connection

--- App5/app/src/main/java/com/aristy/app5/BluetoothCommunication.java
+++ App5/app/src/main/java/com/aristy/app5/BluetoothCommunication.java
@@ -1,2 +1,71 @@
-package com.aristy.app5;public class BluetoothCommunication {
+package com.aristy.app5;
+
+import static com.aristy.app5.HandlerCodes.BT_STATE_DISCONNECTED;
+import static com.aristy.app5.HandlerCodes.BT_STATE_MESSAGE_RECEIVED;
+
+import android.bluetooth.BluetoothSocket;
+import android.os.Handler;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class BluetoothCommunication extends Thread {
+
+    private InputStream inputStream = null;
+    private OutputStream outputStream = null;
+
+    BluetoothConnection bluetoothConnection;
+    private BluetoothSocket bluetoothSocket;
+    Handler handler;
+
+    private static final String TAG_BT_COM = "app5_BT_Communication";
+
+    public BluetoothCommunication(BluetoothConnection bluetoothConnection, Handler handler){
+        this.bluetoothConnection = bluetoothConnection;
+        this.handler = handler;
+        this.bluetoothSocket = bluetoothConnection.getBluetoothSocket();
+        try {
+            inputStream = bluetoothSocket.getInputStream();
+            outputStream = bluetoothSocket.getOutputStream();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+            Log.e(TAG_BT_COM, "Error : ", exception);
+        }
+    }
+
+    @Override
+    public void run() {
+        byte[] buffer = new byte[1024];
+        int bytes;
+
+        while(bluetoothSocket.isConnected()){
+            try {
+                bytes = inputStream.read(buffer);
+                String tempMessage = new String(buffer,0, bytes);
+                handler.obtainMessage(BT_STATE_MESSAGE_RECEIVED, tempMessage).sendToTarget();
+            } catch (IOException exception) {
+                exception.printStackTrace();
+                Log.e(TAG_BT_COM, "BT disconnect from decide end. : ", exception);
+                try {
+                    if (bluetoothSocket != null && bluetoothSocket.isConnected()){
+                        bluetoothSocket.close();
+                    }
+                    handler.obtainMessage(BT_STATE_DISCONNECTED).sendToTarget();
+                } catch (IOException ioException){
+                    ioException.printStackTrace();
+                }
+            }
+            bluetoothSocket = bluetoothConnection.getBluetoothSocket();
+        }
+    }
+
+    public void write(byte[] bytes){
+        try {
+            outputStream.write(bytes);
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
 }
--- App5/app/src/main/java/com/aristy/app5/BluetoothConnection.java
+++ App5/app/src/main/java/com/aristy/app5/BluetoothConnection.java
@@ -1,4 +1,54 @@
 package com.aristy.app5;
 
-public class BleutoothConnection {
+import static com.aristy.app5.HandlerCodes.BT_STATE_CONNECTED;
+import static com.aristy.app5.HandlerCodes.BT_STATE_CONNECTION_FAILED;
+
+import android.annotation.SuppressLint;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.UUID;
+
+public class BluetoothConnection extends Thread {
+
+    private static final UUID myUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+    private BluetoothSocket bluetoothSocket = null;
+    public Handler handler;
+
+    private static final String TAG_BT_CON = "app5_BT_Connection";
+
+    @SuppressLint("MissingPermission")
+    public BluetoothConnection (BluetoothDevice bluetoothDevice, Handler handler){
+        this.handler = handler;
+        try {
+            Log.d(TAG_BT_CON, "Creating socket, my uuid : " + myUUID);
+            bluetoothSocket = bluetoothDevice.createRfcommSocketToServiceRecord(myUUID);
+        } catch (IOException exception) {
+            exception.printStackTrace();
+            Log.e(TAG_BT_CON, "Error : ", exception);
+        }
+    }
+
+    public BluetoothSocket getBluetoothSocket(){
+        return bluetoothSocket;
+    }
+
+    @SuppressLint("MissingPermission")
+    @Override
+    public void run(){
+        Message message = Message.obtain();
+        try {
+            bluetoothSocket.connect();
+            message.what = BT_STATE_CONNECTED;
+        } catch (IOException exception) {
+            exception.printStackTrace();
+            Log.e(TAG_BT_CON, "Error : ", exception);
+            message.what = BT_STATE_CONNECTION_FAILED;
+        }
+        handler.sendMessage(message);
+    }
 }
