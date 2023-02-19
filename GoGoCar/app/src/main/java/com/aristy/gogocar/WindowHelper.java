3b459dc49add7aef4eef7c229f77ca692146fcc4
Aristylux <axelmezade@orange.fr>
2023-02-19 22:00:05 +0100

    add comment class

--- GoGoCar/app/src/main/java/com/aristy/gogocar/WindowHelper.java
+++ GoGoCar/app/src/main/java/com/aristy/gogocar/WindowHelper.java
@@ -5,14 +5,26 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
-// ---- WINDOW settings ----
+/**
+ * ---- WINDOW settings ----
+ */
 public class WindowHelper {
 
+    /**
+     * Set Window option for status bar
+     * @param activity  current activity
+     * @param window    current window
+     */
     public static void setWindowVersion(Activity activity, Window window){
         window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR | View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR);
         setWindowFlag(activity, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
     }
 
+    /**
+     * Set parameter
+     * @param activity  current activity
+     * @param bits      parameter
+     */
     public static void setWindowFlag(Activity activity, final int bits) {
         Window win = activity.getWindow();
         WindowManager.LayoutParams winParams = win.getAttributes();
