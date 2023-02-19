5cf11572ec64cc3545ba9995dd2fc05447e1b7bd
Aristylux <axelmezade@orange.fr>
2023-02-19 10:40:36 +0100

    implement lib and dlider lib

--- GoGoCar/app/src/main/java/com/aristy/gogocar/SliderActivity.java
+++ GoGoCar/app/src/main/java/com/aristy/gogocar/SliderActivity.java
@@ -1,2 +1,56 @@
-package com.aristy.gogocar;public class SliderActivity {
+package com.aristy.gogocar;
+
+import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.r0adkll.slidr.Slidr;
+import com.r0adkll.slidr.model.SlidrConfig;
+import com.r0adkll.slidr.model.SlidrPosition;
+
+public class SliderActivity extends AppCompatActivity {
+
+    public static final String ARG_LINK = "link";
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_slider);
+
+        String link = getIntent().getStringExtra(ARG_LINK);
+
+        // Set web content
+        WebView web = findViewById(R.id.web_view);
+
+        web.loadUrl(link);
+
+        // Enable javascript
+        WebSettings webSettings = web.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        // Result state page
+        web.setWebViewClient(new FragmentApp.Callback());
+
+        //web.addJavascriptInterface(new WebInterface(getActivity(), getContext(), web, SQLConnection, userPreferences, handlers), "Android");
+
+
+        // Set slider
+        SlidrConfig config = new SlidrConfig.Builder()
+                .position(SlidrPosition.LEFT)
+                .sensitivity(1f)
+                .scrimColor(Color.TRANSPARENT)
+                .scrimStartAlpha(0f)
+                .scrimEndAlpha(0f)
+                .velocityThreshold(2400)
+                .distanceThreshold(0.25f)
+                .edge(false)
+                .build();
+
+        Slidr.attach(this, config);
+    }
 }
