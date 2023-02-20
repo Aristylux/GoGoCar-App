bf84880214e1006ff591ba61bb1c85d7c5a6f04c
Aristylux <axelmezade@orange.fr>
2023-02-20 14:00:11 +0100

    implement new fragment

--- GoGoCar/app/src/main/java/com/aristy/gogocar/FragmentNav.java
+++ GoGoCar/app/src/main/java/com/aristy/gogocar/FragmentNav.java
@@ -1,2 +1,33 @@
-package com.aristy.gogocar;public class FragmentNav {
+package com.aristy.gogocar;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class FragmentNav extends Fragment {
+
+
+    public FragmentNav () {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_nav, container, false);
+
+
+        return view;
+    }
 }
