6384dd735cbd184e37796835055596ccc1cfd5fd
Aristylux <axelmezade@orange.fr>
2023-04-27 16:40:20 +0200

    create structure of aes key

--- dev/AES/aes.c
+++ dev/AES/aes.c
@@ -5,39 +5,47 @@
 
 #define KEY_256_BITS (size_t) 32 
 
-uint8_t* generate_aes_key(size_t key_size);
-void print_aes_key(uint8_t *key, size_t key_size);
-void free_aes_key(uint8_t *key);
+typedef struct aes_key
+{
+    uint8_t *key;
+    size_t key_size;
+} t_aes_key;
+
+
+t_aes_key* generate_aes_key(size_t key_size);
+void print_aes_key(t_aes_key *key);
+void free_aes_key(t_aes_key *key);
 
 int main() {
     const size_t key_size = 32;  // 256 bits = 32 bytes
-    //uint8_t key[key_size];
 
-    uint8_t *key = generate_aes_key(key_size);
-    print_aes_key(key, key_size);
+    t_aes_key *key = generate_aes_key(key_size);
+    print_aes_key(key);
     free_aes_key(key);
     return 0;
 }
 
-
-uint8_t* generate_aes_key(size_t key_size) {
-    uint8_t *key = (uint8_t*) malloc(key_size);
+t_aes_key* generate_aes_key(size_t key_size) {
+    t_aes_key *key = (t_aes_key*) malloc(sizeof(t_aes_key));
+    key->key_size = key_size;
+    key->key = (uint8_t*) malloc(key_size);
     srand(time(NULL));  // Seed the random number generator
     for (size_t i = 0; i < key_size; i++) {
-        key[i] = rand() % 256;  // Generate a random byte
+        key->key[i] = rand() % 256;  // Generate a random byte
     }
     return key;
 }
 
-void print_aes_key(uint8_t *key, size_t key_size) {
+void print_aes_key(t_aes_key *key) {
     // Print the key in hexadecimal format
     printf("AES key: ");
-    for (size_t i = 0; i < key_size; i++) {
-        printf("%02x", key[i]);
+    for (size_t i = 0; i < key->key_size; i++) {
+        printf("%02x", key->key[i]);
     }
     printf("\n");
 }
 
-void free_aes_key(uint8_t *key){
+void free_aes_key(t_aes_key *key){
+    free(key->key);
     free(key);
 }
