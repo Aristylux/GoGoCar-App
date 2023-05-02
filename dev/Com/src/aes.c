5bf5813ff8389bef3029da77930c0d02c5ea638b
Aristylux <axelmezade@orange.fr>
2023-05-02 16:52:29 +0200

    add algo of ars encryption

--- dev/Com/src/aes.c
+++ dev/Com/src/aes.c
@@ -24,3 +24,87 @@ void free_aes_key(t_aes_key *key){
     free(key->key);
     free(key);
 }
+
+/*
+ *      10 rounds for 128-bit keys.
+ *      12 rounds for 192-bit keys.
+ *      14 rounds for 256-bit keys.
+ */
+
+void aes_encrypt(char *plaintext, t_aes_key *key){
+
+    // add round key
+
+
+    // ENCRYPTION ROUND
+    // sub bytes
+
+    // shift row
+
+    // mix column
+
+    // add round key
+
+
+    // LAST ROUND
+    // sub bytes
+
+    // shift row
+
+    // add round key
+}
+
+void aes_decrypt(char *ciphertext, t_aes_key *key){
+
+    // add round key
+
+    // DECRYPTION ROUND
+    // inv shift row
+
+    // inv sub bytes
+
+    // inv mix column
+
+    // add round key
+
+    
+    // LAST ROUND
+    // inv shift row
+
+    // inv round bytes
+
+    // add round key
+
+}
+
+/**
+ * @brief each byte of the state is combined with a byte of the round key using bitwise xor
+ * 
+ */
+void add_round_key(){
+    
+}
+
+/**
+ * @brief a non-linear substitution step where each byte is replaced with another according to a lookup table
+ * 
+ */
+void sub_bytes(){
+
+}
+
+/**
+ * @brief a transposition step where the last three rows of the state are shifted cyclically a certain number of steps
+ * 
+ */
+void shift_row(){
+
+}
+
+/**
+ * @brief a linear mixing operation which operates on the columns of the state, combining the four bytes in each column.
+ * 
+ */
+void mix_column() {
+
+}
--- dev/Com/src/aes.h
+++ dev/Com/src/aes.h
@@ -3,9 +3,9 @@
 #include <time.h>
 #include <stdint.h>
 
-
-
-#define KEY_256_BITS (size_t) 32 
+#define KEY_128_BITS (size_t) 16
+#define KEY_192_BITS (size_t) 24
+#define KEY_256_BITS (size_t) 32
 
 typedef struct aes_key
 {
@@ -17,3 +17,6 @@ typedef struct aes_key
 t_aes_key* generate_aes_key(size_t key_size);
 void print_aes_key(t_aes_key *key);
 void free_aes_key(t_aes_key *key);
+
+void aes_encrypt(char *plaintext, t_aes_key *key);
+void aes_decrypt(char *ciphertext, t_aes_key *key);
